a8ef4d2dc91f40e3584888c4b1731524f0ea9a6c
Roman Tunkiv <85829645+skif6623@users.noreply.github.com>
2022-10-02 22:14:36 +0300

    create color switcher

--- src/js/01-color-switcher.js
+++ src/js/01-color-switcher.js
@@ -1,1 +1,24 @@
+let timerId = null;
+const refs = {
+  startButton: document.querySelector('[data-start]'),
+  stopButton: document.querySelector('[data-stop]'),
+};
 
+refs.startButton.addEventListener('click', changeBgColor);
+refs.stopButton.addEventListener('click', stopChangeColor);
+
+function changeBgColor() {
+  refs.startButton.setAttribute('disabled', true);
+  timerId = setInterval(() => {
+    document.body.style.backgroundColor = getRandomHexColor();
+  }, 1000);
+}
+
+function stopChangeColor() {
+  refs.startButton.removeAttribute('disabled');
+  clearInterval(timerId);
+}
+
+function getRandomHexColor() {
+  return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
+}
